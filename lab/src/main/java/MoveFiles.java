9179740e3884812bc4e320d1f2f11ed2dd546f45
Roberto Solares <betosolaresgar@gmail.com>
2019-10-01 09:43:45 -0600

    Get the path to the directories

--- lab/src/main/java/MoveFiles.java
+++ lab/src/main/java/MoveFiles.java
@@ -1,3 +1,7 @@
+
+import java.io.File;
+import javax.swing.JFileChooser;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -28,6 +32,11 @@ public class MoveFiles extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         MainMenuButton = new javax.swing.JButton();
+        StartingPath = new javax.swing.JLabel();
+        DestinationPath = new javax.swing.JLabel();
+        StartingDirectoryButton = new javax.swing.JButton();
+        DestinationPathButton = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -40,27 +49,67 @@ public class MoveFiles extends javax.swing.JFrame {
             }
         });
 
+        StartingDirectoryButton.setText("Directorio Inicial");
+        StartingDirectoryButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                StartingDirectoryButtonActionPerformed(evt);
+            }
+        });
+
+        DestinationPathButton.setText("Directorio Final");
+        DestinationPathButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DestinationPathButtonActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Trasladar");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(133, 133, 133)
-                .addComponent(jLabel1)
-                .addContainerGap(136, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(MainMenuButton)
-                .addGap(32, 32, 32))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(133, 133, 133)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(19, 19, 19)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(StartingDirectoryButton)
+                                .addGap(18, 18, 18)
+                                .addComponent(StartingPath))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(DestinationPathButton)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(DestinationPath))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(91, 91, 91)
+                        .addComponent(jButton1)
+                        .addGap(18, 18, 18)
+                        .addComponent(MainMenuButton)))
+                .addContainerGap(98, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(18, 18, 18)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 179, Short.MAX_VALUE)
-                .addComponent(MainMenuButton)
-                .addGap(57, 57, 57))
+                .addGap(55, 55, 55)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(StartingPath)
+                    .addComponent(StartingDirectoryButton))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(DestinationPath)
+                    .addComponent(DestinationPathButton))
+                .addGap(48, 48, 48)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(MainMenuButton)
+                    .addComponent(jButton1))
+                .addGap(43, 43, 43))
         );
 
         pack();
@@ -76,6 +125,35 @@ public class MoveFiles extends javax.swing.JFrame {
         this.setVisible(false);
         this.dispose();
     }//GEN-LAST:event_MainMenuButtonActionPerformed
+
+    /**
+     * Get the starting path.
+     * @param evt The event performed.
+     */
+    private void StartingDirectoryButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_StartingDirectoryButtonActionPerformed
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int option = fileChooser.showOpenDialog(this);
+        if (option == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            StartingPath.setText(file.getPath());
+        }
+
+    }//GEN-LAST:event_StartingDirectoryButtonActionPerformed
+
+    /**
+     * Get the destination path.
+     * @param evt The event performed.
+     */
+    private void DestinationPathButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DestinationPathButtonActionPerformed
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int option = fileChooser.showOpenDialog(this);
+        if (option == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
+            DestinationPath.setText(file.getPath());
+        }
+    }//GEN-LAST:event_DestinationPathButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -107,7 +185,12 @@ public class MoveFiles extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel DestinationPath;
+    private javax.swing.JButton DestinationPathButton;
     private javax.swing.JButton MainMenuButton;
+    private javax.swing.JButton StartingDirectoryButton;
+    private javax.swing.JLabel StartingPath;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
